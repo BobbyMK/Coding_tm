e3d3d9fe29eceac54d4f1e5bdf7dd94bd4a63272
Bobby_MK <m.kafoev@kodland.team>
2022-04-29 16:54:41 +0300

    via prefetch_related

--- response/serializers.py
+++ response/serializers.py
@@ -1,17 +1,10 @@
 from rest_framework import serializers
 from response.models import Food, FoodCategory
 
-class FilteredListSerializer(serializers.ListSerializer):
-
-    def to_representation(self, data):
-        data = data.filter(is_publish=True)
-        return super(FilteredListSerializer, self).to_representation(data)
-
 class FoodSerializer(serializers.ModelSerializer):
     additional = serializers.SlugRelatedField(many=True, read_only=True, slug_field='internal_code')
 
     class Meta:
-        list_serializer_class = FilteredListSerializer
         model = Food
         fields = ('internal_code', 'code', 'name_ru', 'description_ru', 'description_en',
                   'description_ch', 'is_vegan', 'is_special', 'cost', 'additional')
--- response/views.py
+++ response/views.py
@@ -1,9 +1,18 @@
 from rest_framework import generics
+from django.db.models import Prefetch
 
-from response.models import FoodCategory
+from response.models import FoodCategory, Food
 from response.serializers import FoodListSerializer
 
 class FoodCategoryListAPIView(generics.ListAPIView):
 
     serializer_class = FoodListSerializer
-    queryset = FoodCategory.objects.filter(food__is_publish=True).distinct()
+    def get_queryset(self):
+        queryset = FoodCategory.objects.filter(food__is_publish=True).distinct().prefetch_related(
+            Prefetch('food', queryset=Food.objects.filter(is_publish=True))
+        )
+        print(queryset.query)
+        return queryset
+
+
+
